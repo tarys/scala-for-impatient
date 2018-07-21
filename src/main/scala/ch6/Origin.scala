3de9c72df7135cf6e75ed3492778f74d554f98b0
Taras Slipets <taras.slipets@gmail.com>
2018-07-21 15:08:13 +0200

    Chapter 6 / Exercise 3

---
+++ src/main/scala/ch6/Origin.scala
@@ -0,0 +1,10 @@
+package ch6
+
+/**
+  * java.awt.Point class has constructors with parameters,
+  * but object construction in Scala does not allow to specify
+  * constructors parameters for objects.
+  */
+object Origin extends java.awt.Point {
+
+}
