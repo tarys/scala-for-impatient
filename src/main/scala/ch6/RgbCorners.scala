0ce603921cc83d922b220f4855136450e6bca24a
Taras Slipets <taras.slipets@gmail.com>
2018-07-21 16:18:22 +0200

    Chapter 6 / Exercise 8

---
+++ src/main/scala/ch6/RgbCorners.scala
@@ -0,0 +1,14 @@
+package ch6
+
+/**
+  *
+  */
+object RgbCorners extends Enumeration {
+  val Black = Value(0x000000)
+  val White = Value(0xffffff)
+  val Red = Value(0xff0000)
+  val Green = Value(0x00ff00)
+  val Blue = Value(0x0000ff)
+  val Cyan = Value(0x00ffff)
+  val Magenta = Value(0xff00ff)
+}
