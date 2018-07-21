1d14f709067d4dc3078aa55df10969c2757d1532
Taras Slipets <taras.slipets@gmail.com>
2018-07-21 16:06:27 +0200

    Chapter 6 / Exercise 6

---
+++ src/main/scala/ch6/CardSuits.scala
@@ -0,0 +1,11 @@
+package ch6
+
+/**
+  *
+  */
+object CardSuits extends Enumeration {
+  val Spade = Value("♠")
+  val Club = Value("♣")
+  val Diamond = Value("♦")
+  val Heart = Value("♥")
+}
---
+++ src/main/scala/ch6/Runner.scala
@@ -0,0 +1,8 @@
+package ch6
+
+/**
+  *
+  */
+object Runner extends App {
+  CardSuits.values.foreach(cs => print(cs.toString + ","))
+}
