6887c1ee81a21b041c74eb95809a05e552d5e9d9
Taras Slipets <taras.slipets@gmail.com>
2018-07-21 14:56:32 +0200

    Chapter 6 / Exercise 1

---
+++ src/main/scala/ch6/Conversions.scala
@@ -0,0 +1,7 @@
+package ch6
+
+object Conversions {
+  def inchesToCentimeters(inches: Double): Double = 0.0
+  def gallonsToLiters(inches: Double): Double = 0.0
+  def milesToKilometers(inches: Double): Double = 0.0
+}
