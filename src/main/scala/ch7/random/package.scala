5cf7456673f5f6e6c2ce55b24657b7309f5c32c0
Taras Slipets <taras.slipets@gmail.com>
2018-07-22 19:11:53 +0200

    Chapter 7 / Exercise 3

---
+++ src/main/scala/ch7/random/package.scala
@@ -0,0 +1,18 @@
+package ch7
+
+package object random {
+  var next = 1
+  val a = 1664525
+  val b = 1013904223
+  val n = 32
+
+  def nextInt(): Int = {
+    next = (next * a + b) % math.pow(2, n).toInt
+  }
+
+  def nextDouble(): Double = nextInt()
+
+  def setSeed(seed: Int): Unit = {
+    this.next = seed
+  }
+}
